f50ec5c946ecffb9051fb2c92bac2a6c332c9aab
rKadirV <rafitutus123@gmail.com>
2023-06-02 17:36:51 -0400

    renamed:    estilos/estilocartelera.css -> estilos/estilos-pelicula.css 	modified:   estilos/estilos.css 	modified:   index.html 	new file:   pelicula.html 	new file:   script/datos.js

---
+++ script/datos.js
@@ -0,0 +1,16 @@
+function datos(id){
+    switch (id){
+        case 1:
+            titulo = "La Ex"
+        break;
+        case 2:
+            return "";
+        break;
+        case 3:
+            return "";
+        break;
+        case 4:
+            return "";
+        break;
+    }
+}
