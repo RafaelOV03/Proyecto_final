b97c7384b40d6a10dd59ddf4e9ea88a8db378911
rKadirV <rafitutus123@gmail.com>
2023-06-02 18:01:19 -0400

    modified:   estilos/estilos-pelicula.css 	modified:   pelicula.html 	modified:   script/datos.js

--- script/datos.js
+++ script/datos.js
@@ -1,7 +1,16 @@
 function datos(id){
     switch (id){
         case 1:
-            titulo = "La Ex"
+            document.getElementById("titulo").textContent = 
+            "La Ex";
+            document.getElementById("descripcion").textContent = 
+            "A los 16 años, Sasha puso en Internet la foto de su supuesta novia, una chica espectacular, para presumir ante sus amigos. Años después, y a punto de casarse, la chica de la foto reaparece para convertir su vida en una pesadilla.";
+            document.getElementById("duracion").textContent = 
+            "Duracion: 1h 30m";
+            document.getElementById("tiempo").textContent = 
+            "Fecha de estreno: 11 de marzo del 2021";
+            document.getElementById("trailer").src = 
+            "https://www.youtube.com/embed/6iIgkTLGV9M";
         break;
         case 2:
             return "";
@@ -13,4 +22,9 @@ function datos(id){
             return "";
         break;
     }
+    document.getElementById("portada").src = "pelicula/cartelera/"+id+"/portada.jpg";
+    document.getElementById("avance1").src = "pelicula/cartelera/"+id+"/avances/1.png";
+    document.getElementById("avance2").src = "pelicula/cartelera/"+id+"/avances/2.png";
+    document.getElementById("avance3").src = "pelicula/cartelera/"+id+"/avances/3.png";
+    document.getElementById("avance4").src = "pelicula/cartelera/"+id+"/avances/4.png";
 }
